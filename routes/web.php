b2f8dbbc14d94861051af0fc9265210974677e8b
Aliefhasyani <aliefhasyani05@gmail.com>
2025-08-24 20:13:54 +0800

    feat. middleware and role based dashboard setup

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Container\Attributes\Auth;
+use Illuminate\Support\Facades\Route;
+use PharIo\Manifest\Author;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/admin/dashboard',[AdminController::class,'dashboard'])->middleware(['auth','role:admin'])->name('admin.dashboard');
+Route::get('/author/dashboard',[AuthorController::class,'dashboard'])->middleware(['auth','role:author'])->name('author.dashboard');
+
+require __DIR__.'/auth.php';
